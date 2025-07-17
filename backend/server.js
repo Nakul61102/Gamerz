61a5cb42f03736c49625aa9635a782912ef0e74a
Nakul61102 <nakul358panchal@gmail.com>
2025-07-17 19:32:08 +0530

    Chaning in variables

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*", // frontend URL
+    origin: process.env.FRONTEND_URL, // frontend URL
     methods: ["GET", "POST"],
   },
 });
--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,5 @@
 // socket.js
 import { io } from "socket.io-client";
 
-const socket = io(process.env.BACKEND_URL || "http://localhost:3001"); // Replace with your backend URL
+const socket = io(process.env.REACT_APP_BACKEND_URL); // Replace with your backend URL
 export default socket;
